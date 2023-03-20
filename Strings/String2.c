b7aa717feb9a14184e064c980078b87f96ed9903
die-mirarchi <diemirar@gmail.com>
2023-03-20 11:15:26 -0300

    2 str commit

--- Strings/String2.c
+++ Strings/String2.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+#define WORD_LEN 100
+
+int len(const char arr[]){
+    int i = 0;
+    while (arr[i] != '\0') i++;
+    return i;
+}
+
 int main() {
-    printf("Hello, World!\n");
+    char Word[WORD_LEN] = { 0 }, Inverse[WORD_LEN] = { 0 };
+    fgets(Word, sizeof(Word), stdin);
+    for (int i = 0; i < len(Word); i++) {
+        Inverse[i] = Word[len(Word) - i - 1];
+    }
+    printf("%s\n", Inverse);
     return 0;
 }
