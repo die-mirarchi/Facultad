23bddaca1c79f33b4210374aa5cab440770be6ab
die-mirarchi <diemirar@gmail.com>
2023-03-20 09:41:38 -0300

    1 str commit

--- Strings/String1.c
+++ Strings/String1.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+#define WORD_LEN 100
+
 int main() {
-    printf("Hello, World!\n");
+    char Word[WORD_LEN];
+    fgets(Word, sizeof(Word), stdin);
+    for (int i = 0; i < WORD_LEN; ++i) {
+        if (Word[i] >= 97 && Word[i] <= 122) {
+            Word[i] -= 32;
+        }
+    }
+    printf("%s\n", Word);
     return 0;
 }
